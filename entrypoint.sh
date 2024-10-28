f2c178b8accabba45494cf8a12fc295420c4fea2
Joseph Logue <92321711+JosephLogue3@users.noreply.github.com>
2024-10-28 10:40:04 -0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,18 @@ set -e
 git config --global --add safe.directory /github/workspace
 
 git fetch --tags
+# This suppress an error occurred when the repository is a complete one.
 
-# Check if the repository is shallow before unshallowing
-if git rev-parse --is-shallow-repository; then
-  echo "Repository is shallow. Unshallowing..."
-  git fetch --prune --unshallow
-else
-  echo "Repository is complete. Skipping unshallowing."
-fi
+git fetch --prune --unshallow || true
 
 latest_tag=''
 
 if [ "${INPUT_SEMVER_ONLY}" = 'false' ]; then
-  # Get the actual latest tag.
-  # If no tags found, suppress an error. In such case stderr will be not stored in latest_tag variable so no additional logic is needed.
+  # Get a actual latest tag.
+  # If no tags found, supress an error. In such case stderr will be not stored in latest_tag variable so no additional logic is needed.
   latest_tag=$(git describe --abbrev=0 --tags || true)
 else
-  # Get the latest tag in the shape of semver.
+  # Get a latest tag in the shape of semver.
   for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
     tag="${ref#refs/tags/}"
     if echo "${tag}" | grep -Eq '^v?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'; then
@@ -35,4 +30,4 @@ if [ "${latest_tag}" = '' ] && [ "${INPUT_WITH_INITIAL_VERSION}" = 'true' ]; the
   latest_tag="${INPUT_INITIAL_VERSION}"
 fi
 
-echo "tag=${latest_tag}" >> "$GITHUB_OUTPUT"
+echo "tag=${latest_tag}" >> $GITHUB_OUTPUT
